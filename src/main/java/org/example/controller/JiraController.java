9d199fd7429d05218079475e6efae5c58e7901cb
YeowonKIM <kkyoo152@gmail.com>
2025-01-22 17:43:43 +0100

    Feat: Todo CRUD

---
+++ src/main/java/org/example/controller/JiraController.java
@@ -0,0 +1,35 @@
+package org.example.controller;
+
+import org.example.dto.TodoEntity;
+import org.example.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jira")
+public class JiraController {
+    @Autowired
+    TodoRepository todoRepository;
+
+    @GetMapping("/get-all")
+    public List<TodoEntity> getAll() {
+        return todoRepository.findAll();
+    }
+
+    @GetMapping("/get-by-id")
+    public TodoEntity getById(@PathVariable Long id) {
+        return todoRepository.findById(id).get();
+    }
+
+    @PostMapping("/save")
+    public TodoEntity save(@RequestBody TodoEntity todoItem) {
+        return todoRepository.save(todoItem);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable long id) {
+        todoRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/org/example/dto/TodoEntity.java
@@ -0,0 +1,29 @@
+package org.example.dto;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name="todo")
+public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed = false;
+
+}
